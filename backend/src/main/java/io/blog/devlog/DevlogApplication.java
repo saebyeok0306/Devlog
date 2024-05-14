3a29ecf97ce21cae9a0d9ce58480dec977dccb68
JangSeHun <westreed@naver.com>
2024-05-14 15:24:31 +0900

    rename: 네이밍 변경

--- backend/src/main/java/io/blog/devlog/DevlogApplication.java
+++ backend/src/main/java/io/blog/devlog/DevlogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class DevlogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(DevlogApplication.class, args);
 	}
 
 }
