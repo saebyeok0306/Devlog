c5b24c479c90c97e195350d68d235a006f8d1b44
JangSeHun <westreed@naver.com>
2024-05-31 19:21:14 +0900

    feat: add blog info

---
+++ backend/src/main/java/io/blog/devlog/domain/info/repository/InfoRepository.java
@@ -0,0 +1,9 @@
+package io.blog.devlog.domain.info.repository;
+
+import io.blog.devlog.domain.info.model.Info;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InfoRepository extends JpaRepository<Info, Integer> {
+}
