1a5c1014b2137125e5b86f4765cb50bf2886e910
JangSeHun <westreed@naver.com>
2024-06-01 19:49:41 +0900

    feat: graceful shutdown

--- backend/src/main/java/io/blog/devlog/DevlogApplication.java
+++ backend/src/main/java/io/blog/devlog/DevlogApplication.java
@@ -2,12 +2,19 @@ package io.blog.devlog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class DevlogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevlogApplication.class, args);
+//		SpringApplication.run(DevlogApplication.class, args);
+		SpringApplication application = new SpringApplicationBuilder()
+				.sources(DevlogApplication.class)
+				.listeners(new ApplicationPidFileWriter("./application.pid"))
+				.build();
+		application.run(args);
 	}
 
 }
