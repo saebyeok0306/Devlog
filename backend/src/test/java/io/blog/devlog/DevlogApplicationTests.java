eef27909cbbbf90a3990bbfb3717afa340bf7ef6
JangSeHun <westreed@naver.com>
2024-06-01 18:19:13 +0900

    fix: set context test profile to test

--- backend/src/test/java/io/blog/devlog/DevlogApplicationTests.java
+++ backend/src/test/java/io/blog/devlog/DevlogApplicationTests.java
@@ -2,7 +2,9 @@ package io.blog.devlog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class DevlogApplicationTests {
 
