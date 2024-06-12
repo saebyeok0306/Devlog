093ac313be5b4c88cad18df9af9b0c01e6a883cd
JangSeHun <westreed@naver.com>
2024-06-12 23:42:44 +0900

    feat: check login when getting payload

--- frontend/src/utils/authenticate.js
+++ frontend/src/utils/authenticate.js
@@ -1,4 +1,4 @@
-import { Auth } from "recoil/authAtom";
+import { Auth, authAtom } from "recoil/authAtom";
 import { getCookie, removeCookie, setCookie } from "./useCookie";
 import { decodeJWT } from "./useJWT";
 import {
@@ -6,6 +6,7 @@ import {
   REFRESH_TOKEN_STRING,
 } from "constants/user/login";
 import { EMPTY_AUTH } from "constants/user/auth";
+import { useRecoilValue } from "recoil";
 
 export const signIn = (
   accessToken,
@@ -15,14 +16,14 @@ export const signIn = (
 ) => {
   if (accessToken != null && refreshToken != null) {
     const payload = decodeJWT(accessToken);
-    const payload_refresh = decodeJWT(refreshToken);
+    // const payload_refresh = decodeJWT(refreshToken);
     setCookie(ACCESS_TOKEN_STRING, accessToken, {
       path: "/",
-      expires: new Date(payload_refresh.exp * 1000),
+      // expires: new Date(payload_refresh.exp * 1000),
     });
     setCookie(REFRESH_TOKEN_STRING, refreshToken, {
       path: "/",
-      expires: new Date(payload_refresh.exp * 1000),
+      // expires: new Date(payload_refresh.exp * 1000),
     });
     setAuthDto(new Auth(payload.name, payload.email, true));
     alert(message);
@@ -44,17 +45,19 @@ export const reissueToken = (headers) => {
   const refreshToken = getCookie(REFRESH_TOKEN_STRING);
   if (accessToken == null || refreshToken == null)
     throw new Error("토큰이 없습니다.");
-  const payload_refresh = decodeJWT(getCookie(REFRESH_TOKEN_STRING));
+  // const payload_refresh = decodeJWT(getCookie(REFRESH_TOKEN_STRING));
   setCookie(ACCESS_TOKEN_STRING, accessToken, {
     path: "/",
-    expires: new Date(payload_refresh.exp * 1000),
+    // expires: new Date(payload_refresh.exp * 1000),
   });
 };
 
-export const getPayload = () => {
+export const GetPayload = () => {
+  const authDto = useRecoilValue(authAtom);
+  if (!authDto.isLogin) return EMPTY_AUTH;
+
   const token = getCookie(ACCESS_TOKEN_STRING);
-  if (token == null) {
-    return EMPTY_AUTH;
-  }
+  if (token == null) return EMPTY_AUTH;
+
   return decodeJWT(token);
 };
