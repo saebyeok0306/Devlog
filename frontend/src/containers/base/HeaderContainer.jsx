2d94879973b037fdf8925c818122e5f9c3a91120
JangSeHun <westreed@naver.com>
2024-06-12 23:57:58 +0900

    rename: rightMenu

--- frontend/src/containers/base/HeaderContainer.jsx
+++ frontend/src/containers/base/HeaderContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "components/base/header/Header";
 import UserMenu from "components/base/userMenu";
-import RightMenu from "components/rightMenu";
+import RightMenu from "components/right";
 
 function HeaderContainer() {
   return <Header right={<RightMenu />} menu={<UserMenu />} />;
