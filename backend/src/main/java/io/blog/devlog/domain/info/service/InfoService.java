f33bf5d68c945b59290b67bd6b77b6e01ecd6b2f
JangSeHun <westreed@naver.com>
2024-06-28 17:46:03 +0900

    feat: Handle empty info list in InfoService

--- backend/src/main/java/io/blog/devlog/domain/info/service/InfoService.java
+++ backend/src/main/java/io/blog/devlog/domain/info/service/InfoService.java
@@ -4,9 +4,8 @@ import io.blog.devlog.domain.info.dto.RequestInfoDto;
 import io.blog.devlog.domain.info.dto.ResponseInfoDto;
 import io.blog.devlog.domain.info.model.Info;
 import io.blog.devlog.domain.info.repository.InfoRepository;
-import io.blog.devlog.domain.user.model.Role;
 import io.blog.devlog.domain.user.model.User;
-import io.blog.devlog.domain.user.repository.UserRepository;
+import io.blog.devlog.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -20,21 +19,18 @@ import java.util.List;
 @Transactional
 public class InfoService {
     private final InfoRepository infoRepository;
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     public ResponseInfoDto getBlogInfo() {
         Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
         PageRequest pageRequest = PageRequest.of(0, 1, sort);
         List<Info> infoList = infoRepository.findAll(pageRequest).getContent();
-        for (Info info : infoList) {
-            return ResponseInfoDto.toDto(info);
-        }
-        throw new IllegalArgumentException("블로그 정보가 존재하지 않습니다.");
+        return infoList.isEmpty() ? ResponseInfoDto.toNullDto() : ResponseInfoDto.toDto(infoList.get(0));
     }
 
     public boolean createBlogInfo(RequestInfoDto requestInfoDto) {
         // TODO: 관리자 계정을 가져오는 방법을 수정해야함.
-        User user = userRepository.findByRole(Role.ADMIN).orElse(null);
+        User user = userService.getAdmin();
         if (user == null) return false;
         Info info = requestInfoDto.toEntity(user);
         infoRepository.save(info);
