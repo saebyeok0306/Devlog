763f698e2cbcf43de92f59f2ee9327fd1ed9d9d7
JangSeHun <westreed@naver.com>
2024-07-01 18:53:06 +0900

    feat: Refactor JwtServiceConfig to TestConfig and add FileHandler creation method

--- backend/src/test/java/io/blog/devlog/config/TestConfig.java
+++ backend/src/test/java/io/blog/devlog/config/TestConfig.java
@@ -1,9 +1,11 @@
 package io.blog.devlog.config;
 
+import io.blog.devlog.domain.file.handler.FileHandler;
+import io.blog.devlog.domain.file.service.TempFileService;
 import io.blog.devlog.global.jwt.service.JwtService;
 import org.springframework.test.util.ReflectionTestUtils;
 
-public class JwtServiceConfig {
+public class TestConfig {
 
     public JwtService createJwtService() {
         JwtService jwtService = new JwtService();
@@ -12,5 +14,12 @@ public class JwtServiceConfig {
         ReflectionTestUtils.setField(jwtService, "refreshTokenExpiration", 86400);
         jwtService.init();
         return jwtService;
+    }
+
+    public FileHandler createFileHandler(TempFileService tempFileService) {
+        FileHandler fileHandler = new FileHandler(tempFileService);
+        ReflectionTestUtils.setField(fileHandler, "uploadPath", "C:\\Programming\\Blog\\devlog\\backend\\src\\main\\resources\\upload");
+        ReflectionTestUtils.setField(fileHandler, "requestPath", "res");
+        return fileHandler;
     }
 }
