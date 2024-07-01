5626ffb224c3b6ade3d28c0803030b08f5cd3fa8
JangSeHun <westreed@naver.com>
2024-07-01 18:59:18 +0900

    feat: Add findByFileUrl method to TempFileRepository

--- backend/src/main/java/io/blog/devlog/domain/file/repository/TempFileRepository.java
+++ backend/src/main/java/io/blog/devlog/domain/file/repository/TempFileRepository.java
@@ -4,6 +4,9 @@ import io.blog.devlog.domain.file.model.TempFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TempFileRepository extends JpaRepository<TempFile, Long> {
+    public Optional<TempFile> findByFileUrl(String fileUrl);
 }
