40e4ab6598679322f86ca3b3f54bd49ee271cb88
JangSeHun <westreed@naver.com>
2024-07-09 19:56:24 +0900

    refactor: Update LoginSuccessHandler and OAuth2LoginSuccessHandler to use user.updateRefreshToken method

--- backend/src/main/java/io/blog/devlog/global/login/handler/LoginSuccessHandler.java
+++ backend/src/main/java/io/blog/devlog/global/login/handler/LoginSuccessHandler.java
@@ -38,7 +38,8 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
         String refreshToken = jwtService.createRefreshToken(user); // JwtService의 createRefreshToken을 사용하여 RefreshToken 발급
 
         jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken); // 응답 헤더에 AccessToken, RefreshToken 실어서 응답
-        userService.updateRefreshToken(user, refreshToken);
+//        userService.updateRefreshToken(user, refreshToken);
+        user.updateRefreshToken(refreshToken);
 
         log.info("로그인에 성공하였습니다. Email : {}", email);
         log.info("로그인에 성공하였습니다. AccessToken : {}", accessToken);
