f13d764f9b9628a64ce7732b571d72fa13020a65
JangSeHun <westreed@naver.com>
2024-07-18 19:06:21 +0900

    feat: Add support for uploading file  in FileUploader component

--- frontend/src/components/editor/postEditor/fileUploader/FileUploader.jsx
+++ frontend/src/components/editor/postEditor/fileUploader/FileUploader.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
-import { Button, FileInput, Label, Modal } from "flowbite-react";
+import { Button, FileInput, Label, List, Modal } from "flowbite-react";
 import { upload_file_api } from "api/File";
 import { toast } from "react-toastify";
 import { onErrorImg } from "utils/defaultImg";
+
+import { HiOutlineX } from "react-icons/hi";
+
+import "./FileUploader.scss";
 
 function FileUploader({
   close,
@@ -38,16 +42,42 @@ function FileUploader({
     }
     await upload_file_api(file)
       .then((res) => {
-        const fileName = res.data.fileName.replace(/\.[^/.]+$/, "");
-        textApi.replaceSelection(
-          `![${fileName}](${process.env.REACT_APP_API_FILE_URL}/${res.data.filePath}/${res.data.fileUrl})\n`
-        );
+        // TODO: VIDEO를 업로드한 경우에는 따로 처리하기 rehype-video
+        if (res.data.fileType === "IMAGE") {
+          const fileName = res.data.fileName.replace(/\.[^/.]+$/, "");
+          textApi.replaceSelection(
+            `![${fileName}](${process.env.REACT_APP_API_FILE_URL}/${res.data.filePath}/${res.data.fileUrl})\n`
+          );
+        } else if (res.data.fileType === "VIDEO") {
+          textApi.replaceSelection(
+            `${process.env.REACT_APP_API_FILE_URL}/${res.data.filePath}/${res.data.fileUrl}\n`
+          );
+        }
         setFiles([...files, res.data]);
       })
       .catch((err) => {
         toast.error(`파일 업로드에 실패했습니다.\n${err}`);
       });
     closeHandler();
+  };
+
+  const UploadedFiles = () => {
+    const checkFiles = files.filter((file) => file.fileType !== "IMAGE");
+    if (checkFiles.length === 0) {
+      return <></>;
+    }
+    return (
+      <List className="file-upload-files">
+        {checkFiles.map((file, idx) => (
+          <List.Item key={idx} className="file-upload-file">
+            <button className="inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white">
+              <p className="file-upload-file-name">{file.fileName}</p>
+              <HiOutlineX />
+            </button>
+          </List.Item>
+        ))}
+      </List>
+    );
   };
 
   return (
@@ -74,6 +104,7 @@ function FileUploader({
               />
             </div>
           ) : null}
+          <UploadedFiles />
         </Modal.Body>
         <Modal.Footer className="publish-footer">
           <Button onClick={() => uploadHandler()}>첨부하기</Button>
