4b59f5cd761b2a9485cccb23220cfeefcbbb0a23
JangSeHun <westreed@naver.com>
2024-08-15 22:20:29 +0900

    refactor: Update PostCommentContainer to use Recoil for authentication and sort comments

--- frontend/src/containers/post/PostCommentContainer.jsx
+++ frontend/src/containers/post/PostCommentContainer.jsx
@@ -2,14 +2,18 @@ import { get_post_url_api } from "api/Posts";
 import Comment from "components/base/comment";
 import Post from "components/post";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useRecoilState } from "recoil";
+import { authAtom } from "recoil/authAtom";
 import { postAtom } from "recoil/postAtom";
+import { sortComments } from "utils/sortComments";
 
 function PostCommentContainer({ ...props }) {
   console.log("PostCommentContainer", props);
+  const navigate = useNavigate();
   const { postUrl } = useParams();
+  const [authDto] = useRecoilState(authAtom);
   const [, setPostContent] = useRecoilState(postAtom);
   const [comments, setComments] = useState([]);
 
@@ -18,13 +22,14 @@ function PostCommentContainer({ ...props }) {
       .then((res) => {
         console.log(res.data);
         setPostContent(res.data?.post);
-        setComments(res.data?.comments);
+        setComments(sortComments(res.data?.comments));
       })
       .catch((error) => {
         console.log(error);
         toast.error("잘못된 접근입니다.");
+        navigate(-1);
       });
-  }, [postUrl]);
+  }, [postUrl, authDto]);
 
   return (
     <>
