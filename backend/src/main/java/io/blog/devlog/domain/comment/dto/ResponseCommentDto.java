ba7f9a016eb993397e015c47e0ac76e28b0c340c
JangSeHun <westreed@naver.com>
2024-08-15 22:16:05 +0900

    feat: Add id and createdAt fields to ResponseCommentDto

--- backend/src/main/java/io/blog/devlog/domain/comment/dto/ResponseCommentDto.java
+++ backend/src/main/java/io/blog/devlog/domain/comment/dto/ResponseCommentDto.java
@@ -6,20 +6,26 @@ import io.blog.devlog.domain.user.dto.ResponseUserDto;
 import io.blog.devlog.domain.user.model.User;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResponseCommentDto {
+    private Long id;
     private ResponseUserDto user;
     private Long parent; // 대댓글
     private String content;
+    private LocalDateTime createdAt;
 
     public static ResponseCommentDto of(Comment comment) {
         return ResponseCommentDto.builder()
+                .id(comment.getId())
                 .user(ResponseUserDto.of(comment.getUser()))
                 .parent(comment.getParent())
                 .content(comment.getContent())
+                .createdAt(comment.getCreatedAt())
                 .build();
     }
 }
