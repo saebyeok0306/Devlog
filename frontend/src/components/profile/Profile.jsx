1aa8c4abdaff075aec089166b0fbc843827df3ad
JangSeHun <westreed@naver.com>
2024-09-11 17:24:20 +0900

    feat: Add profile skeleton

---
+++ frontend/src/components/profile/Profile.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./Profile.scss";
+
+function Profile() {
+  return <div />;
+}
+
+export default Profile;
--- frontend/src/containers/profile/ProfileContainer.jsx
+++ frontend/src/containers/profile/ProfileContainer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Main from "components/common/main";
 import LeftSideContainer from "containers/side/LeftSideContainer";
+import Profile from "components/profile/Profile";
 
 function ProfileContainer() {
-  return <Main LeftSide={LeftSideContainer} MainContent={<div />} />;
+  return <Main LeftSide={LeftSideContainer} MainContent={Profile} />;
 }
 
 export default ProfileContainer;
