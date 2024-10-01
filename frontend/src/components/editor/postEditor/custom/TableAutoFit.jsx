03c2277c25ab7dc9fcc4680dc3fdbd15d99a686c
JangSeHun <westreed@naver.com>
2024-10-01 18:27:46 +0900

    fix: Modify TableAutoFit to use model instead of view for data

--- frontend/src/components/editor/postEditor/custom/TableAutoFit.jsx
+++ frontend/src/components/editor/postEditor/custom/TableAutoFit.jsx
@@ -12,42 +12,26 @@ export default class TableAutoFit extends Plugin {
       });
 
       button.on("execute", () => {
-        const viewDocument = editor.editing.view.document;
         const model = editor.model;
         const selection = model.document.selection;
         const tableElement = selection.getFirstPosition().findAncestor("table");
         if (!tableElement) {
           return;
         }
-        const selectedTableIdx = tableElement.index;
 
-        editor.editing.view.change((writer) => {
-          for (const viewElement of viewDocument.getRoot().getChildren()) {
-            if (
-              viewElement.is("element", "figure") &&
-              viewElement.index === selectedTableIdx
-            ) {
-              for (const tableChild of viewElement.getChildren()) {
-                if (
-                  tableChild.is("element", "table") &&
-                  tableChild.hasClass("ck-table-resized")
-                ) {
-                  for (const tableAttr of tableChild.getChildren()) {
-                    if (tableAttr.is("element", "colgroup")) {
-                      const cols = Array.from(tableAttr.getChildren());
-                      const rate = 100 / cols.length;
+        editor.model.change((writer) => {
+          const colGroup = tableElement
+            .getChildren()
+            .find((child) => child.name === "tableColumnGroup");
 
-                      // 각 col 요소에 width 스타일 적용
-                      cols.forEach((col) => {
-                        writer.setStyle("width", `${rate}%`, col);
-                      });
-                    }
-                  }
-                }
-              }
-              break;
-            }
+          if (!colGroup) {
+            return;
           }
+          const cols = Array.from(colGroup.getChildren());
+          const rate = 100 / cols.length;
+          colGroup.getChildren().forEach((col) => {
+            writer.setAttribute("columnWidth", `${rate}%`, col);
+          });
         });
       });
 
