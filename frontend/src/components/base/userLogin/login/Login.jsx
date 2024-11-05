b22f927824888802cfee0ec47a6705d5644c4e68
JangSeHun <westreed@naver.com>
2024-11-05 16:54:59 +0900

    feat: Implement Google reCAPTCHA on login page

--- frontend/src/components/base/userLogin/login/Login.jsx
+++ frontend/src/components/base/userLogin/login/Login.jsx
@@ -1,48 +1,58 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Responsive from "components/common/Responsive";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 import "./Login.scss";
 import EmailIcon from "assets/icons/Email";
 import PasswordIcon from "assets/icons/Password";
-import { user_login_api } from "api/User";
+import { user_login_api, user_logout_api, verify_captcha_api } from "api/User";
 import { authAtom } from "recoil/authAtom";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { OAUTH2_URI } from "constants/api/oauth";
 import { signIn } from "utils/authenticate";
 import { toast } from "react-toastify";
+import ReCAPTCHA from "react-google-recaptcha";
+import { themeAtom } from "recoil/themeAtom";
 
 function Login() {
   const navigate = useNavigate();
   const location = useLocation();
+  const isDark = useRecoilValue(themeAtom);
   const { backpath } = location.state || {};
   const [, setAuthDto] = useRecoilState(authAtom);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const captchaRef = useRef(null);
+  const [verify, setVerify] = useState({
+    value: "",
+    step: 0,
+  });
+
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
-      loginAction(event);
+      nextVerifyStep(event);
     }
   };
 
-  const loginAction = (e) => {
+  const nextVerifyStep = (e) => {
     e.preventDefault();
 
     user_login_api(email, password)
       .then(async (res) => {
-        console.log(res);
-        await signIn(setAuthDto);
-        if (backpath) {
-          navigate(backpath);
-        } else {
-          navigate("/");
-        }
+        toast.info(res);
+        user_logout_api();
+        setVerify({ ...verify, step: 1 });
+        setTimeout(() => {
+          setVerify({ ...verify, step: 2 });
+        }, 500);
+        setTimeout(() => {
+          setVerify({ ...verify, step: 3 });
+        }, 1000);
       })
       .catch((err) => {
         toast.error(`${err.response?.data ? err.response.data.error : err}`);
-        console.log(err);
       });
   };
 
@@ -53,6 +63,81 @@ function Login() {
     window.location.href = link;
   };
 
+  const onCaptchaHandler = async (value) => {
+    try {
+      const res = await verify_captcha_api(value);
+      const payload = res.data;
+      if (payload.success) {
+        toast.info("인증되었습니다.");
+        setVerify({ ...verify, value: value });
+        return;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    toast.error("인증에 실패했습니다.");
+    captchaRef.current.props.grecaptcha.reset();
+    setVerify({ ...verify, value: "" });
+  };
+
+  const onCaptchaLoginHandler = (e) => {
+    if (verify.value === "") {
+      toast.error("인증을 먼저 진행해야 합니다.");
+      return;
+    }
+    e.preventDefault();
+    user_login_api(email, password)
+      .then(async (res) => {
+        await signIn(setAuthDto);
+        if (backpath) {
+          navigate(backpath);
+        } else {
+          navigate("/");
+        }
+      })
+      .catch((err) => {
+        toast.error(`${err.response?.data ? err.response.data.error : err}`);
+      });
+  };
+
+  const onCaptchaBackHandler = (e) => {
+    e.preventDefault();
+    setVerify({ ...verify, step: 0 });
+  };
+
+  if (verify.step > 1) {
+    return (
+      <Responsive className="login">
+        <div className="login-box">
+          <div className="title">
+            로그인
+            <div className="liner" />
+          </div>
+          <div
+            className={`verify-align ${verify.step > 2 ? "show-captcha" : "hidden-captcha"}`}
+          >
+            <div className={`captcha-box m-auto`}>
+              <ReCAPTCHA
+                ref={captchaRef}
+                sitekey={`${process.env.REACT_APP_RECAPTCHA_PUBLIC_KEY}`}
+                onChange={onCaptchaHandler}
+                theme={isDark ? "dark" : "light"}
+              />
+            </div>
+            <div className="buttons col w-1/2 m-auto mt-3">
+              <div className="button" onClick={onCaptchaLoginHandler}>
+                로그인 하기
+              </div>
+              <div className="button pick" onClick={onCaptchaBackHandler}>
+                뒤로가기
+              </div>
+            </div>
+          </div>
+        </div>
+      </Responsive>
+    );
+  }
+
   return (
     <Responsive className="login">
       <div className="login-box">
@@ -60,7 +145,7 @@ function Login() {
           로그인
           <div className="liner" />
         </div>
-        <div className="login-align">
+        <div className={`login-align ${verify.step > 0 ? "hidden-login" : ""}`}>
           <div className="login-left">
             <div className="inputs">
               <div className="input">
@@ -91,7 +176,7 @@ function Login() {
           </div>
           <div className="login-right">
             <div className="buttons col">
-              <div className="button" onClick={loginAction} tabIndex={4}>
+              <div className="button" onClick={nextVerifyStep} tabIndex={4}>
                 로그인 하기
               </div>
               <div
