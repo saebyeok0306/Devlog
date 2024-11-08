f0fdc7c8c63803bf02720fdd965129dbcae2197d
JangSeHun <westreed@naver.com>
2024-11-08 19:59:14 +0900

    feat: Improve sitemap generation

--- backend/sitemap-service/src/main/kotlin/io/blog/sitemap/SitemapApplication.kt
+++ backend/sitemap-service/src/main/kotlin/io/blog/sitemap/SitemapApplication.kt
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableScheduling
+//@EnableScheduling
 class SitemapApplication
 
 fun main(args: Array<String>) {
