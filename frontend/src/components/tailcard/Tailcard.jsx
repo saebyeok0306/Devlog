3b3ed071a4fde81cbada9358838173711f79216e
JangSeHun <westreed@naver.com>
2024-12-27 19:48:43 +0900

    feat: Change post author profile

--- frontend/src/components/tailcard/Tailcard.jsx
+++ frontend/src/components/tailcard/Tailcard.jsx
@@ -1,13 +1,12 @@
 ﻿"use client";
 import "./Tailcard.scss";
 import { useRecoilValue } from "recoil";
-import { authAtom } from "@/recoil/authAtom";
 import { SHORT_BLOG_NAME } from "@/constants/base/main";
 import { Avatar } from "flowbite-react";
 import { renderAtom } from "@/recoil/renderAtom";
 
-function Tailcard() {
-  const authDto = useRecoilValue(authAtom);
+function Tailcard({ ...props }) {
+  const { profile } = props;
   const isRender = useRecoilValue(renderAtom);
 
   if (!isRender) {
@@ -17,13 +16,18 @@ function Tailcard() {
   return (
     <div className="tailcard-container">
       <div className="profile">
-        <Avatar className="avatar" img={authDto.profileUrl} size="lg" rounded />
+        <Avatar
+          className="avatar"
+          img={profile?.profileUrl}
+          size="lg"
+          rounded
+        />
       </div>
       <div className="info">
         <p className="name">
-          {authDto.username}｜{SHORT_BLOG_NAME}
+          {profile?.username}｜{SHORT_BLOG_NAME}
         </p>
-        <p className="desc">{authDto?.about || ""}</p>
+        <p className="desc">{profile?.about || "자기소개를 작성해주세요."}</p>
         {/* TODO: github link icon */}
         <ul>
           <li></li>
